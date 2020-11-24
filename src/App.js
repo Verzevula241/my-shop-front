d14f08b28c55f6ba8753f371e39472006ecbcf14
Mike Zapatrin <misha.zapatrin@yandex.ru>
2020-11-24 11:56:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {connect} from "react-redux"
 import {grab} from './actions/clothesAction'
 import React, { useState, useEffect } from 'react';
 import Vitrina from './components/vitrina'
+import {Route, NavLink} from 'react-router-dom'
+import ReactLogo from './img/crown.svg';
 
 
 
@@ -17,6 +19,19 @@ function App(state) {
 
   return (
     <div className="App">
+       <header>
+          <nav className="nav">
+              <img src={CrownLogo} alt=""></img>
+              <ul>
+                  <li>
+                      <NavLink to="/search" className="header-button"><span style={{paddingLeft: "1em"}}>Поиск</span></NavLink>
+                  </li>
+                  <li>
+                      <NavLink to="/video" className="header-button"><span style={{paddingLeft: "1em"}}>Закладки</span></NavLink>
+                  </li>
+              </ul>
+          </nav>
+          </header>
       <button onClick={()=><vitrina data = {state.data}/>}>sdfgldfkglfkdjgghl</button>
       <Vitrina data = {state.data}/>
     </div>
