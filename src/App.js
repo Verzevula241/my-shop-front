e002723a11103c8e3ba4a1af5fb2b1667494c22d
Mike Zapatrin <misha.zapatrin@yandex.ru>
2020-11-25 11:31:43 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import {connect} from "react-redux"
 import {grab} from './actions/clothesAction'
-import React, { useState, useEffect } from 'react';
-import Vitrina from './components/vitrina'
-import {Route, NavLink} from 'react-router-dom'
-import ReactLogo from './img/crown.svg';
+import React, { useEffect } from 'react';
+import Vitrina from './components/Vitrina'
+import {NavLink,Route,Redirect} from 'react-router-dom'
+import CrownLogo from './img/crown.svg';
 
 
 
@@ -20,27 +19,20 @@ function App(state) {
   return (
     <div className="App">
        <header>
-          <nav className="nav">
-              <img src={CrownLogo} alt=""></img>
-              <ul>
-                  <li>
-                      <NavLink to="/search" className="header-button"><span style={{paddingLeft: "1em"}}>Поиск</span></NavLink>
-                  </li>
-                  <li>
-                      <NavLink to="/video" className="header-button"><span style={{paddingLeft: "1em"}}>Закладки</span></NavLink>
-                  </li>
-              </ul>
-          </nav>
+        <NavLink to="/home"><img src={CrownLogo} alt=""/></NavLink>
+        <div style={{display:"flex"}}>
+            <NavLink className="semi-transparent-button" to="/shop">SHOP</NavLink>
+            <NavLink className="semi-transparent-button" to="/contacts">CONTACTS</NavLink>
+        </div>
           </header>
-      <button onClick={()=><vitrina data = {state.data}/>}>sdfgldfkglfkdjgghl</button>
-      <Vitrina data = {state.data}/>
+              <Route path="/home" component={() =>  <Vitrina data = {state.data}/>} />
+              <Redirect from='/' to='/home'/>
     </div>
   );
 }
 
 function mapStateToProps(state){
   return{
-      // videos: state.clothes.clothes,
       data: state.cloathesReducer.clothes
   }
 }
--- src/components/vitrina.js
+++ src/components/vitrina.js
@@ -1,21 +1,46 @@
 import {React} from 'react'
+import './Vitrina.css'
+import {NavLink,Route,Redirect} from 'react-router-dom'
+
 
 function vitrina (state) {
 
     return (
-        <div>
-            {
-                
-          state.data.map((item, index) => {
+        <div className='container'>
+            <div className='container'>
+                {
                     
-          return <div key={index}>
-          <img src={item.items[0].imageUrl} alt='sdf'/>
-          <p>{item.title}</p>
-          </div>
-
+            state.data.slice(0, 3).map((item, index) => {
+                    return  <div  key={index} className="column-small" style={{backgroundImage: `url(${item.items[0].imageUrl})`}}>
+                    <button className="categories-button">
+                        <NavLink style={{ textDecoration: 'none' , color: 'black'}} to={`/${item.routeName}`}>
+                            <span className='title-sale'>{item.title}</span>
+                            <br/>SHOP NOW
+                        </NavLink>
+                    </button>
+                    </div>
+                    }
+                )
                 }
-            )
-            }
+            </div>
+            <div className='container' style={{borderSpacing: '16px'}}>
+                {
+                    state.data.slice(3,5).map((item, index) => {
+
+                        return  <div  key={index} className="column-big" style={{backgroundImage: `url(${item.items[0].imageUrl})`}}>
+                        <button className="categories-button">
+                            <NavLink style={{ textDecoration: 'none' , color: 'black'}} to={`/${item.routeName}`}>
+                                <span className='title-sale'>{item.title}</span>
+                                <br/>SHOP NOW
+                            </NavLink>
+                        </button>
+                        </div>
+
+                    })
+                }
+            
+            </div>
+
         </div>
       );
 
